92c985191b1a3ad5a6ce6872a34f1b68bf795171
chnyangjie <chnyangjie@163.com>
2018-05-09 15:44:49 +0800

    init

---
+++ src/test/java/ren/wizard/archm/uaa/UaaApplicationTests.java
@@ -0,0 +1,16 @@
+package ren.wizard.archm.uaa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UaaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
